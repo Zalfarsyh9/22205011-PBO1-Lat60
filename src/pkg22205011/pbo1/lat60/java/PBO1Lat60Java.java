e41604940e6fa147b7dbcec86c0bec0df6752e13
Zalfarsyh9 <145966652+Zalfarsyh9@users.noreply.github.com>
2023-11-29 13:33:11 +0700

    Update PBO1Lat60Java.java

--- src/pkg22205011/pbo1/lat60/java/PBO1Lat60Java.java
+++ src/pkg22205011/pbo1/lat60/java/PBO1Lat60Java.java
@@ -6,8 +6,11 @@ package pkg22205011.pbo1.lat60.java;
 
 /**
  *
- * @author User Nama : Zalfa Rosiyah Riski Nim : 22205011 Jurusan : Teknik
- * Informatika Deskripsi : Membuat program akatsuki
+ * @author User 
+ * Nama : Zalfa Rosiyah Riski 
+ * Nim : 22205011 
+ * Jurusan : Teknik Informatika 
+ * Deskripsi : Membuat program akatsuki
  */
 class Akatsuki {
 
